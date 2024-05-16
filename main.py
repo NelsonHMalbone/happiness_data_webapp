15e8367d0058ed35abab83fa3edf3974cde328cb
Nelson Malbone <nhmalbone@icloud.com>
2024-05-16 19:30:05 -0400

    scatter plot is completed

--- main.py
+++ main.py
@@ -15,16 +15,31 @@ st.title('Happiness Data App')
 
 # setting up selection boxes
 
-selection_X = st.selectbox("Select data for x-axis", ["GDP", "Happiness", "Generasity"], key='axis-x')
+selection_X = st.selectbox("Select data for x-axis", ["gdp", "happiness", "generosity"], key='axis-x')
 
-selection_Y = st.selectbox("Select data for y-axis", ["GDP", "Happiness", "Generasity"], key='axis-y')
+selection_Y = st.selectbox("Select data for y-axis", ["gdp", "happiness", "generosity"], key='axis-y')
 
+
+
+
+
+match selection_X:
+    case "happiness":
+        x_array = df['happiness']
+    case "generosity":
+        x_array = df['generosity']
+    case "gdp":
+        x_array = df['gdp']
+
+match selection_Y:
+    case "happiness":
+        y_array = df['happiness']
+    case "generosity":
+        y_array = df['generosity']
+    case 'gdp':
+        y_array = df['gdp']
 
 st.subheader(f"{selection_X} and {selection_Y}")
-
-def get_data(data):
-
-
 # adding a plot
-figure = pe.scatter(x=selection_X, y=selection_Y, labels={"x": selection_X, "y": selection_Y})
+figure = pe.scatter(x=x_array, y=y_array, labels={"x": selection_X, "y": selection_Y})
 st.plotly_chart(figure)
